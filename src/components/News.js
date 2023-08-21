8ddb2518957b19bbddc890425d58ccc481f76169
Sachin Saxena <saxenas964@gmail.com>
2023-08-21 22:58:38 +0530

    add loader

--- src/components/News.js
+++ src/components/News.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Newsitem from "./Newsitem";
+import Spinner from "./Spinner";
 const API_KEY = require("./../apikey.json")["key"];
+
 export default class News extends Component {
   constructor() {
     super();
@@ -13,6 +15,7 @@ export default class News extends Component {
     };
   }
   async componentDidMount() {
+    this.setState({ loading: true });
     const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     const parsedData = await data.json();
@@ -20,12 +23,14 @@ export default class News extends Component {
       articles: parsedData.articles,
       articles_length: parsedData.articles.length,
       totalArticles: parsedData.totalResults,
+      loading: false,
     });
   }
   handleNextPage = async () => {
     const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}&page=${
       this.state.page + 1
     }&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     const parsedData = await data.json();
     const new_articles_length =
@@ -37,6 +42,7 @@ export default class News extends Component {
       articles: parsedData.articles,
       page: this.state.page + 1,
       articles_length: new_articles_length,
+      loading: false,
     });
     setTimeout(() => {
       document.body.scrollTop = 0; // For Safari
@@ -48,6 +54,7 @@ export default class News extends Component {
     const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}&page=${
       this.state.page - 1
     }&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     const parsedData = await data.json();
     const new_articles_length =
@@ -57,6 +64,7 @@ export default class News extends Component {
       articles: parsedData.articles,
       page: this.state.page - 1,
       articles_length: new_articles_length,
+      loading: false,
     });
     setTimeout(() => {
       document.body.scrollTop = 0; // For Safari
@@ -68,27 +76,30 @@ export default class News extends Component {
       <>
         <div className="container my-3">
           <h2>Trending now</h2>
+          {console.log("spinner", this.state.loading)}
+          {this.state.loading && <Spinner />}
           <div className="row">
-            {this.state.articles.map((ele) => {
-              return (
-                <div key={ele.url} className="col-md-4 mb-3 mt-2">
-                  <Newsitem
-                    title={
-                      ele.title?.length > 35
-                        ? ele.title.slice(0, 35) + "..."
-                        : ele.title
-                    }
-                    description={
-                      ele.description?.length > 100
-                        ? ele.description.slice(0, 100) + "..."
-                        : ele.description
-                    }
-                    img_url={ele.urlToImage}
-                    url={ele.url}
-                  />
-                </div>
-              );
-            })}
+            {!this.state.loading &&
+              this.state.articles.map((ele) => {
+                return (
+                  <div key={ele.url} className="col-md-4 mb-3 mt-2">
+                    <Newsitem
+                      title={
+                        ele.title?.length > 35
+                          ? ele.title.slice(0, 35) + "..."
+                          : ele.title
+                      }
+                      description={
+                        ele.description?.length > 100
+                          ? ele.description.slice(0, 100) + "..."
+                          : ele.description
+                      }
+                      img_url={ele.urlToImage}
+                      url={ele.url}
+                    />
+                  </div>
+                );
+              })}
           </div>
         </div>
         <div className="d-flex justify-content-between">
